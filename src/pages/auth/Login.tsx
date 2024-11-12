f88e783239c700468b6f8740cb31c7faecf8bcd7
ross <ross@ross.ross>
2024-11-12 17:23:39 +0800

    优化login代码

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
