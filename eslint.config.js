4b1c142ac365b7722cf4cafeba119f7950671fd7
ross <ross@ross.ross>
2025-05-13 18:32:53 +0800

    更新eslint规则

--- eslint.config.js
+++ eslint.config.js
@@ -30,18 +30,8 @@ export default tseslint.config(
       ],
       'no-console': 'error',
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }
 )
